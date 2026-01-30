ac020dfa5e7a3d10fb95543aa065d0fe0aa39911
Mayur-kumbar <mayurkumbhar738@gmail.com>
2026-01-30 13:57:36 +0530

    latest changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! from Express.js');
+  res.send('Hello, World! from Express.js deployed with Docker');
 });
 
 app.listen(PORT,"0.0.0.0", () => {
